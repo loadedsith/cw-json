f58f21223dfd338a12d5b9e9db7279f652e754dc
Jon Petto <jonpetto@gmail.com>
2013-04-15 17:53:54 -0500

    stubbed out games.js

--- games.js
+++ games.js
@@ -5,6 +5,7 @@ $('#show-games').on('click', function() {
   var games = json_data.games;
 
   // Call the display_games function, passing in the games.
+  display_games(games);
 });
 
 // Main function to display all the games.
@@ -12,16 +13,23 @@ var display_games = function(games) {
   var game;
 
   // Slide up (hide) the game list.
+  $('#game-list').slideUp('fast', function() {
     // Empty the game list.
+    $('#game-list').empty();
 
     // Loop through the array of games.
+    for (var i = 0; i < games.length; i++) {
       // Set the game variable equal to the result of calling
       // the build_game function. Be sure to pass in the
       // game currently referenced by the loop.
+      game = build_game(games[i]);
 
-      // Add the recently built game to the list
+      // TODO: Add the recently built game to the list
 
-    // slide down (show) the list of games
+    }
+    // TODO: Slide down (show) the list of games
+
+  });
 };
 
 // Sub function to build the markup for a game.
@@ -30,40 +38,40 @@ var display_games = function(games) {
 var build_game = function(game_data) {
   // Create a variable called game and set equal to a new
   // div element.
+  var game = $('<div>');
 
   // Give the new div a class of 'game'.
+  game.attr('class', 'game');
 
-  // Create variable called img and set it equal to a new
+  // TODO: Create variable called img and set it equal to a new
   // image element.
-  var img = $('<img>').attr('src', game_data.box_art);
 
-  // Set the src attribute of the new image elment to
+  // TODO: Set the src attribute of the new image elment to
   // the box_art property of the game_data object.
 
-  // Append the new image to the new game div.
+  // TODO: Append the new image to the new game div.
 
   // If the game has characters, add a <ul> to the div
   // with an li for each character.
-    // Create a variable named ul and set it equal to a
+  if (game_data.characters.length > 0) {
+    // TODO: Create a variable named ul and set it equal to a
     // new <ul> element.
-    var ul = $('<ul>').attr('class', 'characters');
 
-    // Add a class of 'characters' to the new <ul> element.
+    // TODO: Add a class of 'characters' to the new <ul> element.
 
-    // create a variable named 'li'.
+    // TODO: Create a variable named 'li' to use inside the
+    // following loop.
 
-    // Write a for loop to run once for each character within
+    // TODO: Write a for loop to run once for each character within
     // game_data.
-    for (var i = 0; i < game_data.characters.length; i++) {
-      // Set the variable named 'li' equal to a new <li>
+      // TODO: Set the variable named 'li' equal to a new <li>
       // element. Set the <li>'s text value to the name
       // of the current character.
 
-      // Append the li variable to the <ul> created above.
-    }
+      // TODO: Append the li variable to the <ul> created above.
 
-    // Append the <ul> created above to the game div.
-    game.append(ul);
+    // TODO: Append the <ul> created above to the game div.
+
   }
 
   // Return the completed markup for the game.
